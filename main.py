a7cc296363a790fbaeecd3d7cc55e18eb04aac29
Martin Lüdemann <martin.luedemann78@gmail.com>
2022-07-12 10:18:22 +0200

    Rebuild of the program. Definition of the required functions.

--- main.py
+++ main.py
@@ -1,217 +1,48 @@
-# # defining the necessary functions:
-#
-# def build_structures(input_str, only_rows=False):
-#     # rows
-#     row_1 = list(input_str[0:3])
-#     row_2 = list(input_str[3:6])
-#     row_3 = list(input_str[6:9])
-#
-#     # columns
-#     column_1 = list(input_str[0::3])
-#     column_2 = list(input_str[1::3])
-#     column_3 = list(input_str[2::3])
-#
-#     # diagonals
-#     d_left = list(input_string[::4])
-#     d_right = list(input_string[2:7:2])
-#
-#     # return values: only rows or everything
-#     if only_rows:
-#         return [row_1, row_2, row_3]
-#
-#     else:
-#         return [row_1, row_2, row_3, column_1, column_2, column_3, d_left, d_right]
-#
-#
-# def print_grid(matrix_structure):
-#     print('---------')
-#     print(f'| {" ".join(matrix_structure[0])} |')
-#     print(f'| {" ".join(matrix_structure[1])} |')
-#     print(f'| {" ".join(matrix_structure[2])} |')
-#     print('---------')
-#
-#
-# def check_for_win(matrix_structure):
-#     win_x = any([element.count('X') == 3 for element in matrix_structure])
-#     win_o = any([element.count('O') == 3 for element in matrix_structure])
-#
-#     # function returns 'impossible' if win_x and win_o are both True
-#
-#     if win_x and win_o:
-#         return 'Impossible'
-#
-#     if win_x:
-#         return 'X wins'
-#     if win_o:
-#         return 'O wins'
-#
-#
-# def check_for_draw(matrix_structure, func):
-#     # func -> call of function 'check_for_win', no other function possible
-#
-#     empty_cells = any([item == '_' for element in matrix_structure
-#                        for item in element])
-#
-#     if not empty_cells and func is None:
-#         print('Draw')
-#
-#
-# def check_for_impossible(matrix_structure):
-#     # case where both x and o wins is covered in function "check_for_win"
-#
-#     all_elements = [item for element in matrix_structure for item in element]
-#     count_x = all_elements.count('X')
-#     count_o = all_elements.count('O')
-#
-#     if count_x - count_o >= 2 or count_o - count_x >= 2:
-#         return 'Impossible'
-#
-#
-# def check_for_completion(matrix_structure, func):
-#     empty_cells = any([item == '_' for element in matrix_structure
-#                        for item in element])
-#
-#     if empty_cells and func is None:
-#         return 'Game not finished'
-#
-#
-# # end of section where functions where defined.
-#
-# input_string = input().strip()
-#
-# # game_grid only contains the 3 rows but not all list items required for
-# # checking the conditions needed for check_for_win, check_for_draw and
-# # check_for_completion. It is used for printing the grid and for the function
-# # check_for_draw which requires only the three actual rows for the tests that
-# # are performed by this function
-#
-# game_grid = build_structures(input_string, only_rows=True)
-#
-# # all_conditions contains all possible conditions that have to be checked to
-# # determine if the game is won. This variable is therefore used for the
-# # following functions: check_for_win, check_for_draw and check_for_completion.
-# # It causes an error if it is used in the function check_for_impossible
-#
-# all_conditions = build_structures(input_string, only_rows=False)
-#
-# print_grid(game_grid)
-#
-# if check_for_win(all_conditions):
-#     print(check_for_win(all_conditions))
-#
-# if check_for_draw(all_conditions, check_for_win(all_conditions)):
-#     check_for_draw(all_conditions, check_for_win(all_conditions))
-#
-# if check_for_impossible(game_grid):  # game_grid has to be used as argument!
-#     print(check_for_impossible(game_grid))
-#
-# else:
-#     if check_for_completion(all_conditions, check_for_win(all_conditions)):
-#         print(check_for_completion(all_conditions, check_for_win(all_conditions)))
+def print_field(grid):
+    list_of_fields = [value for value in grid.values()]
+    row_1 = list_of_fields[0:3]
+    row_2 = list_of_fields[3:6]
+    row_3 = list_of_fields[6:9]
 
-def build_structures(input_string, only_rows=False):
-    # rows
-    row_1 = list(input_string[0:3])
-    row_2 = list(input_string[3:6])
-    row_3 = list(input_string[6:9])
-
-    # columns
-    column_1 = list(input_string[0::3])
-    column_2 = list(input_string[1::3])
-    column_3 = list(input_string[2::3])
-
-    # diagonals
-    d_left = list(input_string[::4])
-    d_right = list(input_string[2:7:2])
-
-    # return values: only rows or everything
-    if only_rows:
-        return [row_1, row_2, row_3]
-
-    else:
-        return [row_1, row_2, row_3, column_1, column_2, column_3, d_left, d_right]
-
-
-def print_grid(matrix_structure):
     print('---------')
-    print(f'| {" ".join(matrix_structure[0])} |')
-    print(f'| {" ".join(matrix_structure[1])} |')
-    print(f'| {" ".join(matrix_structure[2])} |')
+    print(f'| {" ".join(row_1)} |')
+    print(f'| {" ".join(row_2)} |')
+    print(f'| {" ".join(row_3)} |')
     print('---------')
 
 
-def create_playground(input_string):
-    playground = {'1 1': input_string[0],
-                  '1 2': input_string[1],
-                  '1 3': input_string[2],
-                  '2 1': input_string[3],
-                  '2 2': input_string[4],
-                  '2 3': input_string[5],
-                  '3 1': input_string[6],
-                  '3 2': input_string[7],
-                  '3 3': input_string[8]}
+def check_for_win(grid):
+    list_of_fields = [value for value in grid.values()]
 
-    return playground
+    if len([item for item in list_of_fields if item == '_']) == 9:
+        return False
+    if len(set(list_of_fields[0:3])) == 1:
+        return True
+    if len(set(list_of_fields[3:6])) == 1:
+        return True
+    if len(set(list_of_fields[6:9])) == 1:
+        return True
+    if len(set([list_of_fields[0], list_of_fields[4], list_of_fields[8]])) == 1:
+        return True
+    if len(set([list_of_fields[2], list_of_fields[4], list_of_fields[6]])) == 1:
+        return True
 
-
-def insert_values(field, input_str):
-    for key, value in field.items():
-        if key == input_str and value == '_':
-            field[key] = 'X'
-            return field
-        if key == input_str and value != '_':
-            return None
+    return False
 
 
-def convert_field_to_string(field):
-    return ''.join([items for items in field.values()])
+def check_for_draw(grid, game_won):
+    list_of_fields = [value for value in grid.values()]
+    return len([item for item in list_of_fields if item == '_']) == 0 and \
+           game_won is False
 
 
-valid_coordinates = ['1 1', '1 2', '1 3',
-                     '2 1', '2 2', '2 3',
-                     '3 1', '3 2', '3 3']
+def set_value(grid, field, turn):
+    for key, value in grid.items():
 
-user_input = input().strip()
+        if key == field and (value == 'X' or value == 'O'):
+            return False
 
-field_of_glory = build_structures(user_input, True)
-structure_for_input = create_playground(user_input)
+        if key == field and (value != 'X' and value != 'O'):
+            grid[key] = turn
 
-print_grid(field_of_glory)
-
-new_structure = {}
-
-while True:
-
-    while True:
-
-        try:
-
-            user_input = input().strip()
-
-            if user_input and (not list(user_input)[0].isnumeric() or not list(user_input)[2].isnumeric()):
-                raise ValueError('no numbers')
-
-            if user_input and user_input not in valid_coordinates:
-                raise IndexError('out of range')
-
-        except ValueError:
-            print('You should enter numbers!')
-
-        except IndexError:
-            print('Coordinates should be from 1 to 3!')
-
-        else:
-            break
-
-    check_for_none = insert_values(structure_for_input, user_input)
-
-    if check_for_none is None:
-        print('This cell is occupied! Choose another one!')
-        continue
-
-    if check_for_none is not None:
-        new_structure = check_for_none
-        string_for_second_print = convert_field_to_string(new_structure)
-        field_for_second_print = build_structures(string_for_second_print, True)
-        print_grid(field_for_second_print)
-        break
+    return grid
